a287934f12b794d272be73562bf8182e5902847c
zhxmdefj <31560362+zhxmdefj@users.noreply.github.com>
2019-03-14 12:49:36 +0800

    Ver0.1

---
+++ Assets/Plugins/ExcelToObject/Codes/JsonToObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using LitJson;
+using UnityEngine;
+
+/* by：xudawang
+   sukiup@163.com*/
+public static class JsonToObject {
+
+    /// <summary>
+    /// 把一个Json文本文件，转成一个对象(Object)
+    /// </summary>
+    /// <typeparam name="T">对象的类型</typeparam>
+    /// <param name="filePath">Json文本文件的地址(需要加上文件名和后缀名)</param>
+    /// <returns></returns>
+    public static List<T> JsonToObject_ByJsonFile<T>(string filePath)
+    {
+        /*直接解析成对象*/
+        //读取Json文本中的内容
+        string json = File.ReadAllText(filePath);
+        //解析Json文本中的内容 -(解析成数组或者List列表都可以)
+        List<T> datas = JsonToObject_ByJsonContent<T>(json);
+
+        return datas;
+    }
+
+
+
+
+
+    /// <summary>
+    /// 把一个Json格式的文本，转成一个对象(Object)
+    /// </summary>
+    /// <typeparam name="T">对象的类型</typeparam>
+    /// <param name="filePath">Json文本中的内容</param>
+    /// <returns></returns>
+    public static List<T> JsonToObject_ByJsonContent<T>(string conntent)
+    {
+        /*直接解析成对象*/
+        //解析Json文本中的内容 -(解析成数组或者List列表都可以)
+        T[] datas = JsonMapper.ToObject<T[]>(conntent);
+
+        //把数组封装成List列表
+        List<T> dataList = new List<T>();
+        for (int i = 0; i < datas.Length; i++)
+        {
+            dataList.Add(datas[i]);
+        }
+
+        return dataList;
+    }
+
+}
---
+++ Assets/Plugins/ExcelToObject/Tools/Excel2Unity/Editor/ExcelUtility.cs
@@ -0,0 +1,286 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Excel;
+using System.Data;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using System;
+
+public class ExcelUtility
+{
+
+	/// <summary>
+	/// 表格数据集合
+	/// </summary>
+	private DataSet mResultSet;
+
+	/// <summary>
+	/// 构造函数
+	/// </summary>
+	/// <param name="excelFile">Excel file.</param>
+	public ExcelUtility (string excelFile)
+	{
+		FileStream mStream = File.Open (excelFile, FileMode.Open, FileAccess.Read);
+		IExcelDataReader mExcelReader = ExcelReaderFactory.CreateOpenXmlReader (mStream);
+		mResultSet = mExcelReader.AsDataSet ();
+	}
+			
+	/// <summary>
+	/// 转换为实体类列表
+	/// </summary>
+	public List<T> ConvertToList<T> ()
+	{
+		//判断Excel文件中是否存在数据表
+		if (mResultSet.Tables.Count < 1)
+			return null;
+		//默认读取第一个数据表
+		DataTable mSheet = mResultSet.Tables [0];
+			
+		//判断数据表内是否存在数据
+		if (mSheet.Rows.Count < 1)
+			return null;
+
+		//读取数据表行数和列数
+		int rowCount = mSheet.Rows.Count;
+		int colCount = mSheet.Columns.Count;
+				
+		//准备一个列表以保存全部数据
+		List<T> list = new List<T> ();
+				
+		//读取数据
+		for (int i=1; i<rowCount; i++) 
+		{
+			//创建实例
+			Type t = typeof(T);
+			ConstructorInfo ct = t.GetConstructor (System.Type.EmptyTypes);
+			T target = (T)ct.Invoke (null);
+			for (int j=0; j<colCount; j++) 
+			{
+				//读取第1行数据作为表头字段
+				string field = mSheet.Rows [0] [j].ToString ();
+				object value = mSheet.Rows [i] [j];
+				//设置属性值
+				SetTargetProperty (target, field, value);
+			}
+					
+			//添加至列表
+			list.Add (target);
+		}
+				
+		return list;
+	}
+
+	/// <summary>
+	/// 转换为Json
+	/// </summary>
+	/// <param name="JsonPath">Json文件路径</param>
+	/// <param name="Header">表头行数</param>
+	public void ConvertToJson (string JsonPath, Encoding encoding)
+	{
+		//判断Excel文件中是否存在数据表
+		if (mResultSet.Tables.Count < 1)
+			return;
+
+		//默认读取第一个数据表
+		DataTable mSheet = mResultSet.Tables [0];
+
+		//判断数据表内是否存在数据
+		if (mSheet.Rows.Count < 1)
+			return;
+
+		//读取数据表行数和列数
+		int rowCount = mSheet.Rows.Count;
+		int colCount = mSheet.Columns.Count;
+
+		//准备一个列表存储整个表的数据
+		List<Dictionary<string, object>> table = new List<Dictionary<string, object>> ();
+
+		//读取数据
+		for (int i = 1; i < rowCount; i++) {
+			//准备一个字典存储每一行的数据
+			Dictionary<string, object> row = new Dictionary<string, object> ();
+			for (int j = 0; j < colCount; j++) {
+				//读取第1行数据作为表头字段
+				string field = mSheet.Rows [0] [j].ToString ();
+				//Key-Value对应
+				row [field] = mSheet.Rows [i] [j];
+			}
+
+			//添加到表数据中
+			table.Add (row);
+		}
+
+		//生成Json字符串
+		string json = JsonConvert.SerializeObject (table, Newtonsoft.Json.Formatting.Indented);
+
+
+	    /*絮大王添加代码：去掉所有的反斜杠+支持数组*/
+	    json = JsonSupportArray(json);
+
+
+		//写入文件
+		using (FileStream fileStream=new FileStream(JsonPath,FileMode.Create,FileAccess.Write)) {
+			using (TextWriter textWriter = new StreamWriter(fileStream, encoding)) {
+				textWriter.Write (json);
+			}
+		}
+	}
+
+	/// <summary>
+	/// 转换为CSV
+	/// </summary>
+	public void ConvertToCSV (string CSVPath, Encoding encoding)
+	{
+		//判断Excel文件中是否存在数据表
+		if (mResultSet.Tables.Count < 1)
+			return;
+
+		//默认读取第一个数据表
+		DataTable mSheet = mResultSet.Tables [0];
+
+		//判断数据表内是否存在数据
+		if (mSheet.Rows.Count < 1)
+			return;
+
+		//读取数据表行数和列数
+		int rowCount = mSheet.Rows.Count;
+		int colCount = mSheet.Columns.Count;
+
+		//创建一个StringBuilder存储数据
+		StringBuilder stringBuilder = new StringBuilder ();
+
+		//读取数据
+		for (int i = 0; i < rowCount; i++) {
+			for (int j = 0; j < colCount; j++) {
+				//使用","分割每一个数值
+				stringBuilder.Append (mSheet.Rows [i] [j] + ",");
+			}
+			//使用换行符分割每一行
+			stringBuilder.Append ("\r\n");
+		}
+
+		//写入文件
+		using (FileStream fileStream = new FileStream(CSVPath, FileMode.Create, FileAccess.Write)) {
+			using (TextWriter textWriter = new StreamWriter(fileStream, encoding)) {
+				textWriter.Write (stringBuilder.ToString ());
+			}
+		}
+
+	}
+
+	/// <summary>
+	/// 导出为Xml
+	/// </summary>
+	public void ConvertToXml (string XmlFile)
+	{
+		//判断Excel文件中是否存在数据表
+		if (mResultSet.Tables.Count < 1)
+			return;
+
+		//默认读取第一个数据表
+		DataTable mSheet = mResultSet.Tables [0];
+
+		//判断数据表内是否存在数据
+		if (mSheet.Rows.Count < 1)
+			return;
+
+		//读取数据表行数和列数
+		int rowCount = mSheet.Rows.Count;
+		int colCount = mSheet.Columns.Count;
+
+		//创建一个StringBuilder存储数据
+		StringBuilder stringBuilder = new StringBuilder ();
+		//创建Xml文件头
+		stringBuilder.Append ("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+		stringBuilder.Append ("\r\n");
+		//创建根节点
+		stringBuilder.Append ("<Table>");
+		stringBuilder.Append ("\r\n");
+		//读取数据
+		for (int i = 1; i < rowCount; i++) {
+			//创建子节点
+			stringBuilder.Append ("  <Row>");
+			stringBuilder.Append ("\r\n");
+			for (int j = 0; j < colCount; j++) {
+				stringBuilder.Append ("   <" + mSheet.Rows [0] [j].ToString () + ">");
+				stringBuilder.Append (mSheet.Rows [i] [j].ToString ());
+				stringBuilder.Append ("</" + mSheet.Rows [0] [j].ToString () + ">");
+				stringBuilder.Append ("\r\n");
+			}
+			//使用换行符分割每一行
+			stringBuilder.Append ("  </Row>");
+			stringBuilder.Append ("\r\n");
+		}
+		//闭合标签
+		stringBuilder.Append ("</Table>");
+		//写入文件
+		using (FileStream fileStream = new FileStream(XmlFile, FileMode.Create, FileAccess.Write)) {
+			using (TextWriter textWriter = new StreamWriter(fileStream,Encoding.GetEncoding("utf-8"))) {
+				textWriter.Write (stringBuilder.ToString ());
+			}
+		}
+	}
+
+	/// <summary>
+	/// 设置目标实例的属性
+	/// </summary>
+	private void SetTargetProperty (object target, string propertyName, object propertyValue)
+	{
+		//获取类型
+		Type mType = target.GetType ();
+		//获取属性集合
+		PropertyInfo[] mPropertys = mType.GetProperties ();
+		foreach (PropertyInfo property in mPropertys) {
+			if (property.Name == propertyName) {
+				property.SetValue (target, Convert.ChangeType (propertyValue, property.PropertyType), null);
+			}
+		}
+	}
+
+
+
+
+
+
+
+    #region 絮大王添加的代码
+    /// <summary>
+    /// 让Json支持数组
+    /// ————————————————————————————————————————————————————
+    /// (这个方法是絮大王自己添加的)
+    /// 此方法是为了让Excel在转化成Json的时候，支持数组  (格式为["元素1","元素2"])
+    /// 
+    /// * 为什么要这样做？
+    ///   在源代码中，转化后的Json字符串是不支持数组的  (转化后，会变成这样：   "[\"元素1\",\"元素2\"]")
+    /// 
+    /// * 此方法的做了什么？
+    ///   把Json字符串传递过来，就会返回一个支持数组的Json字符串  (格式为["元素1","元素2"])
+    /// ——————————————————————————————————————————————————————
+    /// </summary>
+    /// <param name="jsonContent">json的文本内容</param>
+    /// <returns></returns>
+    private string JsonSupportArray(string jsonContent)
+    {
+        //去掉所有的反斜杠  （把"\"替换成""）
+        jsonContent = jsonContent.Replace("\\", string.Empty);
+
+        //为了能够支持数组，所以：
+        //把所有的"[替换成[   
+        //并且把所有的]"替换成]
+        jsonContent = jsonContent.Replace("\"[", "[");
+        jsonContent = jsonContent.Replace("]\"", "]");
+
+        //把所有的".0" 替换成 ""
+        //不然的话，"1"会显示为"1.0"
+        jsonContent = jsonContent.Replace(".0,", ",");
+
+        return jsonContent;
+    }
+
+    #endregion
+}
+
