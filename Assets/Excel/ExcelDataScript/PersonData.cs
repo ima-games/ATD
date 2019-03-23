44dfadff4ff0c1d5b39b35abc57bd2a1f5cb1ca7
1158233061 <1158233061@qq.com>
2019-03-23 21:11:26 +0800

    完成BuffDataBase脚本的基本需求

---
+++ Assets/Excel/ExcelDataScript/PersonData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class PersonData
+{
+    public int ID;
+    public string Name;
+    public int HP;
+    public int HpReturn;
+    public int Attack;
+    public double AttSpeed;
+    public double Speed;
+}
