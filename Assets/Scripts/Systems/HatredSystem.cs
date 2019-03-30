1e67e6dd789c7489ade043cc9a0184f533b2356c
Aery <KillerAery@foxmail.com>
2019-03-30 16:57:28 +0800

    更新个体属性

--- Assets/Scripts/Systems/HatredSystem.cs
+++ Assets/Scripts/Systems/HatredSystem.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class HatredSystem : MonoBehaviour
 {
+    //public Individual GetTarget()
+    //{
+    //    return;
+    //}
+
+
+    private Individual individual;
     // Start is called before the first frame update
     void Start()
     {
-        
+        individual = GetComponent<Individual>();
     }
 
     // Update is called once per frame
