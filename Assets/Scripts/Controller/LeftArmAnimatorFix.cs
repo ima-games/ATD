ce7c6aaf83cca9ae9878634a1ef66be543eb15cd
浩贤 Z <zhxmdefj@outlook.com>
2019-04-14 00:49:11 +0800

    攻击，鼠标视角控制

---
+++ Assets/Scripts/Controller/LeftArmAnimatorFix.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftArmAnimatorFix : MonoBehaviour
+{
+    private Animator animator;
+    public Vector3 a;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK() {
+        if (animator.GetBool("defense") == false) {
+            Transform lefLowerArm = animator.GetBoneTransform(HumanBodyBones.LeftLowerArm);
+            lefLowerArm.localEulerAngles += a;
+            animator.SetBoneLocalRotation(HumanBodyBones.LeftLowerArm, Quaternion.Euler(lefLowerArm.localEulerAngles));
+        }
+    }
+}
