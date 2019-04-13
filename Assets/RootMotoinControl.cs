8a7787fa35d09d00580076daa2fd5b499ed7caa0
浩贤 Z <zhxmdefj@outlook.com>
2019-04-13 20:25:53 +0800

    1. 完善攻击三段动画 通过动画事件ResetTrigger设置攻击输入控制 2. 整理资源路径

---
+++ Assets/RootMotoinControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootMotoinControl : MonoBehaviour
+{
+    private Animator animator;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorMove() {
+        SendMessageUpwards("OnUpdateRM",animator.deltaPosition);
+
+    }
+}
