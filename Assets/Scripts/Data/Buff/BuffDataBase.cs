b95cf360eeb45de0f3894840253c7aa1d59492fb
1158233061 <1158233061@qq.com>
2019-04-14 09:53:00 +0800

    添加BuffDataBase的单例模式以及外部调用数据方法

--- Assets/Scripts/Data/Buff/BuffDataBase.cs
+++ Assets/Scripts/Data/Buff/BuffDataBase.cs
@@ -10,27 +10,54 @@ public class BuffDataBase : MonoBehaviour
     public TextAsset MonsterJ;
     public TextAsset HatredJ;
     public TextAsset BuffDataJ;
+
+    private List<BuffData> buffDatas;
+    private List<PersonData> personDatas;
+    private List<PersonData> perDatas;
+    private List<TowerTrapData> towerTrapDatas;
+    private List<EquipmentData> equipmentDatas;
+    private List<MonsterData> monsterDatas;
+    private List<HatredData> hatredDatas;
+
+    private static BuffDataBase instance;
+    
+    //禁止外界通过new获取该类的实例
+    private BuffDataBase() { }
+
+    /// <summary>
+    /// BuffDataBased的单例模式
+    /// </summary>
+    public static BuffDataBase Instance
+    {
+        get { return instance; }
+        private set { }
+    }
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        List<PersonData> perDatas = JsonToObject.JsonToObject_ByJsonContent<PersonData>(PersonJ.text);
-        List<TowerTrapData> ttDatas = JsonToObject.JsonToObject_ByJsonContent<TowerTrapData>(Tow_TrapJ.text);
-        List<EquipmentData> equipmentDatas = JsonToObject.JsonToObject_ByJsonContent<EquipmentData>(EquipmentJ.text);
-        List<MonsterData> monsterDatas = JsonToObject.JsonToObject_ByJsonContent<MonsterData>(MonsterJ.text);
-        List<HatredData> hatredDatas = JsonToObject.JsonToObject_ByJsonContent<HatredData>(HatredJ.text);
-        List<BuffData> buffDatas = JsonToObject.JsonToObject_ByJsonContent<BuffData>(BuffDataJ.text);
+        perDatas = JsonToObject.JsonToObject_ByJsonContent<PersonData>(PersonJ.text);
+        towerTrapDatas = JsonToObject.JsonToObject_ByJsonContent<TowerTrapData>(Tow_TrapJ.text);
+        equipmentDatas = JsonToObject.JsonToObject_ByJsonContent<EquipmentData>(EquipmentJ.text);
+        monsterDatas = JsonToObject.JsonToObject_ByJsonContent<MonsterData>(MonsterJ.text);
+        hatredDatas = JsonToObject.JsonToObject_ByJsonContent<HatredData>(HatredJ.text);
+        buffDatas = JsonToObject.JsonToObject_ByJsonContent<BuffData>(BuffDataJ.text);
 
-        /*Debug.Log("ID "+perDatas[1].ID);
-        Debug.Log("Name "+perDatas[1].Name);
-        Debug.Log("HP "+perDatas[1].HP);
-        Debug.Log("HpReturn "+perDatas[1].HpReturn);
-        Debug.Log("Attack "+perDatas[1].Attack);
-        Debug.Log("AttSpeed "+perDatas[1].AttSpeed);
-        Debug.Log("Speed "+perDatas[1].Speed);*/
-        //Debug.Log("Name " + hatredDatas[0].State);
-        Debug.Log("ID" + buffDatas[0].ID);
-        Debug.Log("Name" + buffDatas[0].Name);
-        Debug.Log("isIncreaseAttack" + buffDatas[0].isIncreaseAttack);
+        
+    }
+    public BuffData GetBuffData(int ID)
+    {
+        if (ID < 0 || ID >= buffDatas.Count)
+        {
+            Debug.Log("buffDatas列表越界");
+            return null;
+        }
+        return buffDatas[ID];
     }
 
     // Update is called once per frame
@@ -39,3 +66,5 @@ public class BuffDataBase : MonoBehaviour
         
     }
 }
+//该脚本必须挂在一个GameObject上
+//用例：BuffData a = BuffDataBase.Instance.GetBuffData(0);
