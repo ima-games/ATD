7546bfa8807b27af270d97b13e6cc1b3e86a6068
Broker <38775739JOhn@users.noreply.github.com>
2019-04-15 17:18:24 +0800

    初步完成Buff结构

--- Assets/Scripts/Data/Buff/Buff.cs
+++ Assets/Scripts/Data/Buff/Buff.cs
@@ -1,4 +1,7 @@
 ﻿public struct Buff
 {
-
+    public int ID;              //BuffData的ID
+    public float duration;      //持续时间
+    public int repeatCount;     //重复次数
+    public bool isTrigger;      //是否触发类型
 }
