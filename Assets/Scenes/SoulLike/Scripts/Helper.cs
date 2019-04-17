b588f811fae4249760f2e40e2039d3c6e473fc95
zhxmdefj@qq.com <31560362+zhxmdefj@users.noreply.github.com>
2019-04-17 16:26:24 +0800

    改ignore忽略idea文件夹，soullike初步

--- Assets/Scenes/SoulLike/Scripts/Helper.cs
+++ Assets/Scenes/SoulLike/Scripts/Helper.cs
@@ -6,8 +6,10 @@ namespace SA
 {
     public class Helper : MonoBehaviour
     {
-        [Range(0, 1)]
+        [Range(-1, 1)]
         public float vertical;
+        [Range(-1, 1)]
+        public float horizontal;
 
         public bool playAnim;
         public string[] oh_attacks;
@@ -16,6 +18,8 @@ namespace SA
         public bool twoHanded;
         public bool enableRM;
         public bool useItem;
+        public bool interacting;
+        public bool lockon;
 
         Animator anim;
 
@@ -27,18 +31,31 @@ namespace SA
         // Update is called once per frame
         void Update() {
 
-
             enableRM = !anim.GetBool("canMove");
             anim.applyRootMotion = enableRM;
+
+            interacting = anim.GetBool("interacting");
+
+            if(lockon == false) {
+                horizontal = 0;
+                vertical = Mathf.Clamp01(vertical);
+            }
+
+            anim.SetBool("lockon", lockon);
 
             if (enableRM) {
                 return;
             }
 
             if (useItem) {
-                playAnim = false;
-                twoHanded = false;
+
                 anim.Play("use_item");
+                useItem = false;
+            }
+
+            if (interacting) {
+                playAnim = false;
+                vertical = Mathf.Clamp(vertical, 0, 0.5f);
             }
 
             anim.SetBool("twoHanded", twoHanded);
@@ -50,11 +67,17 @@ namespace SA
                 if (!twoHanded) {
                     int r = Random.Range(0, oh_attacks.Length);
                     targetAnim = oh_attacks[r];
+
+                    if (vertical > 0.5f)
+                        targetAnim = "oh_attack_3";
                 }
                 else {
                     int r = Random.Range(0, th_attacks.Length);
                     targetAnim = th_attacks[r];
                 }
+
+                if (vertical > 0.5f)
+                    targetAnim = "oh_attack_3";
 
                 vertical = 0;
 
@@ -64,8 +87,7 @@ namespace SA
                 playAnim = false;
             }
             anim.SetFloat("vertical", vertical);
-
-            useItem = false;
+            anim.SetFloat("horizontal", horizontal);
         }
     }
 }
