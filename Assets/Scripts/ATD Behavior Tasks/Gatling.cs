04d87a66b8306bd3f489092cab8bb2ea348021d1
1158233061 <1158233061@qq.com>
2019-04-19 20:11:03 +0800

    修改BuffDataBase

---
+++ Assets/Scripts/ATD Behavior Tasks/Gatling.cs
@@ -0,0 +1,33 @@
+﻿using BehaviorDesigner.Runtime.Tasks.Basic.UnityGameObject;
+using System.Collections;
+using UnityEngine;
+
+namespace BehaviorDesigner.Runtime.Tasks
+{
+    [TaskDescription("机枪塔的攻击行为")]
+    [TaskCategory("Tower Attack Model")]
+    [TaskIcon("Assets/Behavior Designer Movement/Editor/Icons/{SkinColor}gatlingIcon.png")]
+    public class Gatling : Action
+    {
+        public SharedTransform target;
+        public GameObject bullet;
+        public GameObject bulletPoint;
+        public float bulletSpeed;
+        public float attackRate;
+        
+        IEnumerator Attack()
+        {
+            GameObject bulletObj = GameObject.Instantiate(bullet, 
+                bulletPoint.transform.position, Quaternion.identity);
+            Vector3 
+            bulletObj.GetComponent<Rigidbody>().velocity = transform.TransformDirection
+                (Vector3.forward * bulletSpeed);
+            yield return new WaitForSeconds(attackRate);
+        }
+        public override void OnStart()
+        {
+            StartCoroutine(Attack());
+        }
+    }
+}
+
