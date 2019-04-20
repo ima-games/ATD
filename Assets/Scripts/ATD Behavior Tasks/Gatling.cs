cd9f6351d4dcc9d6838c8a63d2c69c2f1314e865
1158233061 <1158233061@qq.com>
2019-04-20 14:56:19 +0800

    添加BuffDataBase单例的prefab

--- Assets/Scripts/ATD Behavior Tasks/Gatling.cs
+++ Assets/Scripts/ATD Behavior Tasks/Gatling.cs
@@ -17,16 +17,38 @@ namespace BehaviorDesigner.Runtime.Tasks
         
         IEnumerator Attack()
         {
-            GameObject bulletObj = GameObject.Instantiate(bullet, 
-                bulletPoint.transform.position, Quaternion.identity);
-            Vector3 fireDirection = bulletPoint.transform.position - target.Value.position;
-            bulletObj.GetComponent<Rigidbody>().velocity = transform.TransformDirection
-                (fireDirection * bulletSpeed);
+            if (target.Value.tag == "Enemy")
+            {
+                GameObject bulletObj = GameObject.Instantiate(bullet,
+                    bulletPoint.transform.position, Quaternion.identity);
+                Vector3 fireDirection = bulletPoint.transform.position - target.Value.position;
+                bulletObj.GetComponent<Rigidbody>().velocity = transform.TransformDirection
+                    (fireDirection * bulletSpeed);
+            }
+            
             yield return new WaitForSeconds(attackRate);
         }
+
         public override void OnStart()
         {
-            StartCoroutine(Attack());
+            //StartCoroutine(Attack());
+        }
+        public override TaskStatus OnUpdate()
+        {
+            if (target.Value == null)
+            {
+                return TaskStatus.Failure;
+            }
+            if (target.Value.tag == "Enemy")
+            {
+                GameObject bulletObj = GameObject.Instantiate(bullet,
+                    bulletPoint.transform.position, Quaternion.identity);
+                Vector3 fireDirection = target.Value.position - bulletPoint.transform.position;
+                bulletObj.GetComponent<Rigidbody>().velocity = transform.TransformDirection
+                    (fireDirection * bulletSpeed);
+                return TaskStatus.Success;
+            }
+            return TaskStatus.Failure;
         }
     }
 }
