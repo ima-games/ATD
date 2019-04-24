a168c506523547c66522bd485e56d2ba27c459d6
1158233061 <1158233061@qq.com>
2019-04-24 21:17:22 +0800

    更新行为树插件为1.6版，更新策划案的防御塔设计

---
+++ Assets/Behavior Designer/Runtime/Variables/SharedVector3Int.cs
@@ -0,0 +1,12 @@
+#if !UNITY_2017_1
+using UnityEngine;
+
+namespace BehaviorDesigner.Runtime
+{
+    [System.Serializable]
+    public class SharedVector3Int : SharedVariable<Vector3Int>
+    {
+        public static implicit operator SharedVector3Int(Vector3Int value) { return new SharedVector3Int { mValue = value }; }
+    }
+}
+#endif
--- Assets/Scripts/ATD Behavior Tasks/CanSeeEnemy.cs
+++ Assets/Scripts/ATD Behavior Tasks/CanSeeEnemy.cs
@@ -10,33 +10,18 @@ namespace BehaviorDesigner.Runtime.Tasks
     [TaskIcon("Assets/Behavior Designer Movement/Editor/Icons/{SkinColor}CanSeeObjectIcon.png")]
     public class CanSeeEnemy : Conditional
     {
-
-        [Tooltip("Should the 2D version be used?")]
-        public bool usePhysics2D;
-        [Tooltip("The object that we are searching for. If this value is null then the objectLayerMask will be used")]
         public SharedTransform targetObject;
-        [Tooltip("The LayerMask of the objects that we are searching for")]
         public LayerMask objectLayerMask;
-        [Tooltip("The field of view angle of the agent (in degrees)")]
         public SharedFloat fieldOfViewAngle = 90;
-        [Tooltip("The distance that the agent can see ")]
         public SharedFloat viewDistance = 1000;
-        [Tooltip("The offset relative to the pivot position")]
         public SharedVector3 offset;
-        [Tooltip("The object that is within sight")]
         public SharedTransform objectInSight;
 
         // Returns success if an object was found otherwise failure
         public override TaskStatus OnUpdate()
         {
-            if (usePhysics2D)
-            {
-                objectInSight.Value = MovementUtility.WithinSight2D(transform, offset.Value, fieldOfViewAngle.Value, viewDistance.Value, objectLayerMask,"Enemy");
-            }
-            else
-            {
-                objectInSight.Value = MovementUtility.WithinSight(transform, offset.Value, fieldOfViewAngle.Value, viewDistance.Value, objectLayerMask, "Enemy");
-            }
+            objectInSight.Value = MovementUtility.WithinSight(transform, offset.Value, fieldOfViewAngle.Value, viewDistance.Value, objectLayerMask, "Enemy");
+          
 
             if (objectInSight.Value != null)
             {
@@ -62,11 +47,8 @@ namespace BehaviorDesigner.Runtime.Tasks
             {
                 return;
             }
-
-            bool is2D = false;
-            is2D = usePhysics2D;
-
-            MovementUtility.DrawLineOfSight(Owner.transform, offset.Value, fieldOfViewAngle.Value, viewDistance.Value, is2D);
+            
+            MovementUtility.DrawLineOfSight(Owner.transform, offset.Value, fieldOfViewAngle.Value, viewDistance.Value, false);
         }
     }
 }
