20846c3cf59c6a955e833d28fe96b9ada7be1e63
1158233061 <1158233061@qq.com>
2019-04-28 14:38:52 +0800

    添加fireBall行为树任务

---
+++ Assets/Scripts/ATD Behavior Tasks/FireBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorDesigner.Runtime.Tasks
+{
+    [TaskDescription("火焰塔的攻击行为")]
+    [TaskCategory("Tower Attack Model")]
+    [TaskIcon("Assets/Behavior Designer Movement/Editor/Icons/{SkinColor}FireBallIcon.png")]
+    public class FireBall : Action
+    {
+        public SharedTransform target;
+        public GameObject bullet;
+        public GameObject bulletPoint;
+        public float bulletSpeed;
+        public float attackRate;
+
+        private Individual master;          //寄主
+        private bool attacking = false;     //攻击正在进行中
+    }
+}
+
+
