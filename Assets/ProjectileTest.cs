6d44bd81342d7149e5d6c64f2161a8e5163839be
1158233061 <1158233061@qq.com>
2019-04-29 18:28:45 +0800

    添加Mission的json文件，修改防御塔设计

---
+++ Assets/ProjectileTest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileTest : MonoBehaviour
+{
+    public GameObject target;   //要到达的目标
+    public GameObject point;
+    public GameObject bullet;
+    public float speed = 10;    //速度
+    public int rate = 1;
+
+
+
+    void Start()
+    {
+        //计算两者之间的距离
+        
+        StartCoroutine(StartShoot());
+    }
+
+    IEnumerator StartShoot()
+    {
+        GameObject bulletObj = Instantiate(bullet, point.transform.position, Quaternion.identity);
+        float L = (point.transform.position - target.transform.position).x;
+        float H = point.transform.position.y;
+        float a = speed * speed * H * 2 / (L * L);
+        Debug.Log("a:" + a);
+        Debug.Log("L:" + L);
+        Debug.Log("H:" + H);
+        Debug.Log("T:" + L / speed);
+
+        //bulletObj.transform.LookAt(target.transform);
+        bulletObj.GetComponent<Rigidbody>().velocity = Vector3.left * speed;
+        bulletObj.GetComponent<Rigidbody>().AddForce(Vector3.down * a);
+
+        Debug.Log("velocity:" + bulletObj.GetComponent<Rigidbody>().velocity);
+        Debug.Log("Force:" + Vector3.down * a);
+        yield return new WaitForSeconds(rate);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
