753ca399d5592e97650b7a6a1de3846bcea53db7
Aery <530846680@qq.com>
2019-05-29 00:59:54 +0800

    重新整理文件目录

---
+++ Assets/Scripts/IndividualObject/Skill/DirSkill.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// 指向性技能-暂时不做
+/// </summary>
+public class DirSkill : ISkill
+{
+    public void InitSkill(Individual master)
+    {
+
+    }
+
+    public void ReleaseSkill(Individual master)
+    {
+
+    }
+
+    public void UpdateSkill(Individual master)
+    {
+
+    }
+}
