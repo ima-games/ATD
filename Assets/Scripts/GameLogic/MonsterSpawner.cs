877c900d3a1fa53466af19cac336785be9462a7c
Aery <530846680@qq.com>
2019-07-01 10:24:15 +0800

    完善关卡数据读取，完成配置生成怪物队列功能

--- Assets/Scripts/GameLogic/MonsterSpawner.cs
+++ Assets/Scripts/GameLogic/MonsterSpawner.cs
@@ -29,11 +29,13 @@ public class MonsterSpawner : MonoBehaviour
     波数信息
     */
 
+    //波数信息
     private class WaveInformation
     {
-        public List<MonsterList> monsterList;
+        public List<MonsterList> monsterLists;
         public float time;
     }
+
     //怪物列表类
     private class MonsterList
     {
@@ -44,14 +46,15 @@ public class MonsterSpawner : MonoBehaviour
     }
 
     //关卡数据
-    private Dictionary<string, List<WaveInformation>> levelData = new Dictionary<string, List<WaveInformation>>();
-    //关卡时间队列
-    private Queue<float> timeQueue = new Queue<float>();
-    //怪物列表队列
-    private Queue<MonsterList> monsterListQueue = new Queue<MonsterList>();
-    //怪物生成队列
-    private Queue<GameObject> monsterQueue = new Queue<GameObject>();
+    private Dictionary<string, JsonData> levelData = new Dictionary<string, JsonData>();
     
+    //关卡列表队列
+    private Queue<WaveInformation> waveQueue = new Queue<WaveInformation>();
+
+    public GameObject[] monsterPrefabs;
+    public Transform Monsters;
+    public Transform spawnPoint;
+    private WayPointManager wayPointManager;
 
     //获取json文件，传入json文件路径
     private void GetJsonToLevelData(string path)
@@ -70,113 +73,82 @@ public class MonsterSpawner : MonoBehaviour
         string jsonToString = streamReader.ReadToEnd();
 
         //获得json中的数据
-        levelData = JsonMapper.ToObject<Dictionary<string, List<WaveInformation>>>(jsonToString);
-        
-        if(levelData==null)
+        levelData = JsonMapper.ToObject<Dictionary<string, JsonData>>(jsonToString);
+
+        if (levelData==null)
         {
             Logger.Log("没有获取到 关卡json 文件", LogType.Data);
         }
         else
         {
             Logger.Log("已获取到关卡json数据", LogType.Data);
-        }
-    }
-
-    //遍历关卡数据，获取时间队列
-    private void GetTimeQueue()
-    {
-        foreach(WaveInformation t in levelData["misson"])
-        {
-            timeQueue.Enqueue(t.time);
-        }
-        Logger.Log("成功获取时间队列", LogType.Monster);
-    }
-
-    //获取怪物列表队列
-    private void GetMonsterListQueue()
-    {
-        foreach (WaveInformation t in levelData["misson"])
-        {
-            foreach (MonsterList k in t.monsterList)
+            //获取关卡队列
+            var missions = levelData["mission"];
+            for(int i = 0 ;i < missions.Count ;++i)
             {
-                monsterListQueue.Enqueue(k);
+                //获取某个关卡的怪物列表
+                JsonData monsterLists = missions[i]["monsterList"];
+                List<MonsterList> onelist = new List<MonsterList>();
+                for (int j = 0; j< monsterLists.Count; ++j)
+                {
+                    MonsterList m = new MonsterList();
+                    m.monsterID = (int)monsterLists[j]["monsterID"];
+                    m.rate = (float)monsterLists[j]["rate"];
+                    m.wayID = (int)monsterLists[j]["wayID"];
+                    m.count = (int)monsterLists[j]["count"];
+
+                    onelist.Add(m);
+                }
+                //生成某个关卡的数据
+                WaveInformation waveInformation = new WaveInformation();
+                waveInformation.time = (int)missions[i]["time"];
+                waveInformation.monsterLists = onelist;
+
+                waveQueue.Enqueue(waveInformation);
+            }
+
+            Logger.Log("成功获取关卡队列:" + waveQueue.Count, LogType.Monster);
+        }
+    }
+
+    //依次生成怪物
+    IEnumerator CreateMonster()
+    {
+        //每一波
+        while (waveQueue.Count > 0)
+        {
+            WaveInformation waveInformation = waveQueue.Dequeue();
+            //每一波生成前的等待时间
+            yield return new WaitForSeconds(waveInformation.time);
+            //每一个怪物列表
+            foreach (var monsterList in waveInformation.monsterLists)
+            {
+                for (int j = 0; j < monsterList.count; ++j)
+                {
+                    //生成怪物
+                    var monster = Instantiate(monsterPrefabs[monsterList.monsterID-1], spawnPoint.position, Quaternion.identity, Monsters);
+                    //设置路径
+                    monster.GetComponent<BehaviorTree>().SetVariableValue("Road", wayPointManager.GetRoad(monsterList.wayID-1));
+                    //生成间隔
+                    float rate = monsterList.rate;
+                    yield return new WaitForSeconds(rate);
+                }
             }
         }
-
-        Logger.Log("成功获取怪物列表队列", LogType.Monster);
     }
 
-    //访问队列第一个列表，压入生成队列
-    private void GetMonsterListToMonsterQueue()
-    {
-        if(monsterListQueue.Count==0)
-        {
-            Debug.Log("怪物列表队列已为空");
-            return;
-        }
-
-        MonsterList nowMonsterList = monsterListQueue.Peek();
-        Logger.Log("即将生成ID为" + nowMonsterList.monsterID + "的怪物，数量为" + nowMonsterList.count + "，出生点为" + nowMonsterList.wayID + "生成间隔" + nowMonsterList.rate + " s", LogType.Monster);
-
-        for(int i=0;i<nowMonsterList.count;i++)
-        {
-            //需要一个通过ID获取怪物gameObject的方法
-            //monsterQueue.Enqueue(?????(nowMonsterList.monsterID));
-        }
-
-    }
-
-    //通过生成队列依次生成怪物
-    private void CreateMonster()
-    {
-        float rate = monsterListQueue.Peek().rate;
-        //生成队列未空一直生成，生成完了倒计时
-        while(monsterQueue.Count>0)
-        {
-            Instantiate(monsterQueue.Dequeue());
-            new WaitForSeconds(rate);
-        }
-    }
-
-
-    //TODO
-    float timer = 1.0f;
-    public GameObject[] monsterPrefabs;
-    public Transform Monsters;
-    public Transform spawnPoint;
-    private WayPointManager wayPointManager;
 
     private void Awake()
     {
         wayPointManager = GetComponent<WayPointManager>();
+
+        GetJsonToLevelData("Assets/Scripts/TestScripts_Zhidai/Mission.json");
     }
 
     private void Start()
     {
-        //test
-        GetJsonToLevelData("Assets/Scripts/TestScripts_Zhidai/Mission.json");
+        // 开启生成
+        StartCoroutine(CreateMonster());
     }
 
-    private void Update()
-    {
-        if (timer > 0)
-        {
-            timer -= Time.deltaTime;
-        }
-
-        if (timer < 0)
-        {
-            Test();
-            timer = 3.0f;
-        }
-
-    }
-
-
-
-    private void Test()
-    {
-       var monster = Instantiate(monsterPrefabs[Random.Range(0,monsterPrefabs.Length)],spawnPoint.position,Quaternion.identity,Monsters);
-        monster.GetComponent<BehaviorTree>().SetVariableValue("Road",wayPointManager.GetRoad(Random.Range(0,2)));
-    }
 }
