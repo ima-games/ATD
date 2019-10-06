1c813b2c5c39fd770e3944f3b7bda4f0104c911e
Aery <530846680@qq.com>
2019-10-07 00:48:04 +0800

    修改场景+添加限制编辑器渲染脚本

---
+++ Assets/Scripts/Tools/EditorOnlyRender.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorOnlyRender : MonoBehaviour
+{
+    private Renderer renderer = null;
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+        if (!renderer) return;
+        renderer.enabled = false;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
