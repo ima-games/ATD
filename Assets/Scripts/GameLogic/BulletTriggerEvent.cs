c177025b942dfb9dbf5551203f5b8b2da25cdada
Aery <530846680@qq.com>
2019-10-13 01:57:57 +0800

    添加特效

--- Assets/Scripts/GameLogic/BulletTriggerEvent.cs
+++ Assets/Scripts/GameLogic/BulletTriggerEvent.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+//TODO!!
 public class BulletTriggerEvent : MonoBehaviour
 {
     //发射炮弹的塔
     public Individual tower;
 
     //可能的炮弹添加BUFF
-    public int buffID;
+    public int buffID = 0;
+
+    //是否范围性
+    public bool ifRange = false;
+    //范围大小
+    public float range = 3.0f;
 
     //死亡产生的特效对象（该对象会自动删除）
     public GameObject dieEffect;
@@ -18,17 +23,35 @@ public class BulletTriggerEvent : MonoBehaviour
     {
         var collisonObject = collison.gameObject;
 
-        //子弹打到玩家、非个体单位
-        if (collison.name == "PlayerHandle" || LayerMask.LayerToName(collisonObject.layer) != "Individual")
+        //个体子弹
+        if (!ifRange)
         {
-            return;
+            //子弹打到玩家、非个体单位
+            if (collison.name == "PlayerHandle" || LayerMask.LayerToName(collisonObject.layer) != "Individual")
+            {
+                return;
+            }
+
+            //此处应该使用tower的消息系统来发消息
+            MessageSystem messageSystem = tower.GetComponent<MessageSystem>();
+            Individual otherIndividual = collisonObject.GetComponent<Individual>();
+
+            messageSystem.SendMessage(1, otherIndividual.ID, tower.attack);
+
+            if(buffID != 0)
+            {
+                messageSystem.SendMessage(2, otherIndividual.ID,buffID);
+            }
+        }
+        //范围子弹 TODO
+        else
+        {
+            Factory.TraversalIndividualsInCircle(
+             (individual) => { tower.GetComponent<MessageSystem>().SendMessage(2, individual.ID, buffID); }
+                , transform.position, range);
         }
 
-        //此处应该使用tower的消息系统来发消息
-        MessageSystem messageSystem = tower.GetComponent<MessageSystem>();
-        Individual otherIndividual = collisonObject.GetComponent<Individual>();
 
-        messageSystem.SendMessage(1, otherIndividual.ID,tower.attack);
 
         //特效对象产生
         if (dieEffect)
